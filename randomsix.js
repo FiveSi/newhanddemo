7a2bf3f55c4c573f0ba5d9dc1146e2f954198a4c
sdip15fa <91375035+sdip15fa@users.noreply.github.com>
2021-11-30 22:54:05 +0800

    Update randomsix.js

--- randomsix.js
+++ randomsix.js
@@ -1,9 +1,11 @@
 let list = [];
+let r = 50;
 for (i = 1; i < 50; i++) {
   list[i-1] = i;
 }
 for (i = 0; i < 6; i++) {
-  let o = Math.floor(Math.random() * 50);
+  let o = Math.floor(Math.random() * r);
   console.log(list[o]);
   list.splice(o, 1);
+  r--;
 }
