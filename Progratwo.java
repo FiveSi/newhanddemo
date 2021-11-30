65420b68ba524b2025cca588ee188df319d0a47e
FiveSi <95166661+FiveSi@users.noreply.github.com>
2021-11-30 09:39:56 +0800

    Create Progratwo.java

---
+++ Progratwo.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Progratwo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guest Number 0 to 100?");
+        Random guNu = new Random(); 
+        int randomNumber = guNu.nextInt(101);
+        int nAn = 100;
+        int nBn =  0;
+        while (true) {
+            int uNu = scanner.nextInt();
+
+            if (uNu == randomNumber ) {
+                System.out.println("BINGO");
+                break;
+            };
+            if (uNu > randomNumber) {
+                nAn = uNu;
+                System.out.println("The number range is :" + nBn + " to " +  nAn );
+            };
+            if (uNu < randomNumber) {
+                nBn = uNu;    
+                System.out.println("The number range is :" + nBn + " to " +  nAn );
+            };
+          }
+    }
+}
